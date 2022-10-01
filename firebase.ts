47544c4596a5b1cfb8253256c077f393b4c9dfdc
hezekiah02 <olahex@gmail.com>
2022-10-01 01:12:39 +0100

    first commit

---
+++ firebase.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "netflix-28505.firebaseapp.com",
+  projectId: "netflix-28505",
+  storageBucket: "netflix-28505.appspot.com",
+  messagingSenderId: "958939948621",
+  appId: process.env.NEXT_PUBLIC_API_ID
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore()
+const auth = getAuth()
+
+export default app
+export { auth, db }
---
+++ typings.d.ts
@@ -0,0 +1,41 @@
+export interface Genre {
+    id: number
+    name: string
+  }
+  
+  export interface Movie {
+    title: string
+    backdrop_path: string
+    media_type?: string
+    release_date?: string
+    first_air_date: string
+    genre_ids: number[]
+    id: number
+    name: string
+    origin_country: string[]
+    original_language: string
+    original_name: string
+    overview: string
+    popularity: number
+    poster_path: string
+    vote_average: number
+    vote_count: number
+  }
+  
+  export interface Element {
+    type:
+      | 'Bloopers'
+      | 'Featurette'
+      | 'Behind the Scenes'
+      | 'Clip'
+      | 'Trailer'
+      | 'Teaser'
+  }
+  
+   // recoilRoot.d.ts
+   export type RecoilRootProps = {
+    initializeState?: (mutableSnapshot: MutableSnapshot) => void,
+    override?: true,
+    children: React.ReactNode,
+   } | {override: false, children: React.ReactNode, };
+  
